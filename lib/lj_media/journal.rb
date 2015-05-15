003d641e94c6702b3fb4f1a55d49837a2d1e09ad
Denis Misiurca <infoman1985@gmail.com>
2015-05-16 02:19:33 +0300

    Add basic parsers for LJ journal feed and posts

---
+++ lib/lj_media/journal.rb
@@ -0,0 +1,12 @@
+module LJMedia
+  class Journal
+    attr_reader :feed
+
+    def initialize(username)
+      feed_url = "http://#{username}.livejournal.com/data/rss?unfold_embed=1"
+      feed_xml = Feedjira::Feed.fetch_raw feed_url
+
+      @feed = Feedjira::Feed.parse_with JournalParser, feed_xml
+    end
+  end
+end
---
+++ lib/lj_media/journal_parser.rb
@@ -0,0 +1,16 @@
+module LJMedia
+  class JournalParser
+    include SAXMachine
+    include Feedjira::FeedUtilities
+
+    element  :"lj:journalid",   as: :id,        class: Integer
+    element  :"lj:journaltype", as: :type
+    element  :"lj:journal",     as: :name
+    element  :title
+    element  :description
+    element  :link
+    element  :lastBuildDate,    as: :published
+
+    elements :item,             as: :posts,     class: PostParser
+  end
+end
